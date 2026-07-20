7343d57d6e749e746a7615f2ce5819a0f2af5f5a
Francisco Schmidt <franschmidt2415@gmail.com>
2026-07-20 13:20:13 -0300

    Giphy API request

--- src/app/gifs/pages/trending-page/trending-page.ts
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { GifsList } from "../../components/gifs-list/gifs-list";
+import { GifService } from '../../services/gifs';
 
 const imageUrls: string[] = [
     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
@@ -23,4 +24,5 @@ const imageUrls: string[] = [
 })
 export default class TrendingPage {
   gifs = signal(imageUrls);
+  gifService = inject(GifService);
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,9 @@ export const environment = {
   companySlogan: 'Maneja tus Gifs',
 
   // API KEYS
+  giphyApiKey:'ZfpyBwEDX8K6htakNC9PYSU9Siq43Eyl',
+
 
   // URLs
+  giphyUrl: 'https://api.giphy.com/v1',
 };
